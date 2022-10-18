2ab122446cbd0b2c3d0353dff72f4b259ebc578e
elakhfif <akaouam1@gmail.com>
2022-10-18 20:25:55 +0100

    7ARZAN-42

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,7 +6,7 @@
 /*   By: elakhfif <elakhfif@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/11 15:37:16 by elakhfif          #+#    #+#             */
-/*   Updated: 2022/10/18 19:31:39 by elakhfif         ###   ########.fr       */
+/*   Updated: 2022/10/18 20:21:53 by elakhfif         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,28 +14,28 @@
 
 char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 {
-    size_t    i;
-    size_t    j;
+	size_t	i;
+	size_t	j;
 
-    if (needle[0] == '\0')
-        return ((char *)haystack);
-    if (haystack == NULL && len == 0)
-        return (NULL);
-    i = 0;
-    while (haystack[i] != '\0')
-    {
-        j = 0;
-        while (haystack[i + j] == needle[j] && i + j < len)
-        {
-            if (needle[j + 1] == '\0')
-                return ((char *)&haystack[i]);
-            if (haystack[i + j + 1] != needle[j + 1])
-                break ;
-            j++;
-        }
-        i++;
-    }
-    return (NULL);
+	if (needle[0] == '\0')
+		return ((char *)haystack);
+	if (haystack == NULL && len == 0)
+		return (NULL);
+	i = 0;
+	while (haystack[i] != '\0')
+	{
+		j = 0;
+		while (haystack[i + j] == needle[j] && i + j < len)
+		{
+			if (needle[j + 1] == '\0')
+				return ((char *)&haystack[i]);
+			if (haystack[i + j + 1] != needle[j + 1])
+				break ;
+			j++;
+		}
+		i++;
+	}
+	return (NULL);
 }
 /*
 #include <stdio.h>
